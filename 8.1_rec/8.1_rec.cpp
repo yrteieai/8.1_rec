d1f832454e3ebd590cb77305af137c221bae8f2c
Anya Stefurak <anyutastefurak@gmail.com>
2024-10-27 16:30:23 +0200

    first master commit

---
+++ 8.1_rec/8.1_rec.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+
+// Рекурсивна функція для знаходження індексу третьої крапки
+int FindThirdDot(char* str, int i = 0, int dotCount = 0) {
+    if (str[i] == '\0') {
+        return -1; // якщо третя крапка не знайдена
+    }
+    if (str[i] == '.') {
+        dotCount++;
+        if (dotCount == 3) {
+            return i; // повертаємо індекс третьої крапки
+        }
+    }
+    return FindThirdDot(str, i + 1, dotCount); // рекурсивний виклик
+}
+
+// Рекурсивна функція для заміни кожного четвертого символу на крапку
+void ReplaceEveryFourth(char* str, int i = 0) {
+    if (str[i] == '\0') {
+        return; // вихід з рекурсії, якщо кінець рядка
+    }
+    if ((i + 1) % 4 == 0) {
+        str[i] = '.'; // заміна кожного четвертого символу на крапку
+    }
+    ReplaceEveryFourth(str, i + 1); // рекурсивний виклик
+}
+
+int main() {
+    char str[101];
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+
+    // Викликаємо функцію для знаходження індексу третьої крапки
+    int thirdDotIndex = FindThirdDot(str);
+    if (thirdDotIndex != -1) {
+        cout << "Index of third dot: " << thirdDotIndex << endl;
+    }
+    else {
+        cout << "The string does not contain three dots." << endl;
+    }
+
+    // Викликаємо функцію для заміни кожного четвертого символу на крапку
+    ReplaceEveryFourth(str);
+    cout << "Modified string: " << str << endl;
+
+    return 0;
+}
---
+++ UnitTest_8.1_rec/UnitTest_8.1_rec.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../8.1_rec/8.1_rec.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81rec
+{
+	TEST_CLASS(UnitTest81rec)
+	{
+	public:
+
+		TEST_METHOD(TestFindThirdDot)
+		{
+			char testStr1[] = "a.b.c.d"; 
+			Assert::AreEqual(5, FindThirdDot(testStr1));
+
+			char testStr2[] = "ab.cd.ef.gh"; 
+			Assert::AreEqual(8, FindThirdDot(testStr2));
+
+			char testStr3[] = "ab.c"; 
+			Assert::AreEqual(-1, FindThirdDot(testStr3));
+		}
+	};
+}
